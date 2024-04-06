1f738f33047752f9c32a4df79cc2071da48c611c
yana-asds <yanakonshina@gmail.com>
2024-04-06 20:57:52 +0100

    Problem Set 4

---
+++ problemSets/PS04/my_answers/PS4_answers_YK_23359606.R
@@ -0,0 +1,43 @@
+#####################
+# load libraries
+# set wd
+# clear global .envir
+#####################
+
+# remove objects
+rm(list=ls())
+# detach all libraries
+detachAllPackages <- function() {
+  basic.packages <- c("package:stats", "package:graphics", "package:grDevices", "package:utils", "package:datasets", "package:methods", "package:base")
+  package.list <- search()[ifelse(unlist(gregexpr("package:", search()))==1, TRUE, FALSE)]
+  package.list <- setdiff(package.list, basic.packages)
+  if (length(package.list)>0)  for (package in package.list) detach(package,  character.only=TRUE)
+}
+detachAllPackages()
+
+# load libraries
+pkgTest <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[,  "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg,  dependencies = TRUE)
+  sapply(pkg,  require,  character.only = TRUE)
+}
+
+# here is where you load any necessary packages
+# ex: stringr
+# lapply(c("stringr"),  pkgTest)
+
+lapply(c("survival", "eha", "tidyverse", "ggfortify", "stargazer"),  pkgTest)
+
+
+# Loading data
+data(child)
+summary(child)
+
+# Creating a survival object for survival analysis
+child_surv <- with(child, Surv(enter, exit, event))
+summary(child_surv)
+
+# Estimating duration Cox Proportional Hazard regression model 
+child_surv_CoxPH <- coxph(child_surv ~ m.age + sex, data = child)
+stargazer(child_surv_CoxPH)
